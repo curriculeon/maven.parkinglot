c5b2d464adf752c67f2ea3ac58493d1c2f4aa994
git-leon <xleonhunter@gmail.com>
2025-04-01 15:36:14 -0400

    update

--- src/test/java/com/github/curriculeon/parkinglot/car/GetSizeTest.java
+++ src/test/java/com/github/curriculeon/parkinglot/car/GetSizeTest.java
@@ -3,43 +3,25 @@ package com.github.curriculeon.parkinglot.car;
 import com.github.curriculeon.parkinglot.Car;
 import com.github.curriculeon.parkinglot.Size;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class GetSizeTest {
-    
     @Test
-    public void testGetSmallCarSize() {
-        // Given
-        final Car car = new Car(Size.SMALL);
-        
-        // When
-        final Size size = car.getSize();
-        
-        // Then
-        assertEquals("Car size should be SMALL", Size.SMALL, size);
+    public void test() {
+        for (Size size : Size.values()) {
+            test(size);
+        }
     }
 
-    @Test
-    public void testGetMediumCarSize() {
+    public void test(Size expectedSize) {
         // Given
-        final Car car = new Car(Size.MEDIUM);
-        
-        // When
-        final Size size = car.getSize();
-        
-        // Then
-        assertEquals("Car size should be MEDIUM", Size.MEDIUM, size);
-    }
+        final Car car = new Car(expectedSize);
 
-    @Test
-    public void testGetLargeCarSize() {
-        // Given
-        final Car car = new Car(Size.LARGE);
-        
         // When
-        final Size size = car.getSize();
-        
+        final Size actualSize = car.getSize();
+
         // Then
-        assertEquals("Car size should be LARGE", Size.LARGE, size);
+        assertEquals("Car size should be LARGE", expectedSize, actualSize);
     }
 } 
